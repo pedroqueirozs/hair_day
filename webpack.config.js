99ad746a0bbb1efc3d8e529181453f1304d8a0cd
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-02-05 19:18:44 -0300

    Adicionando o babel

--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/, //Essa é uma regra para pegar todos os arquivos .cc
+        test: /\.css$/, //Essa é uma regra para pegar todos os arquivos .css
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
